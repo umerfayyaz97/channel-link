5d33516cbdd50e943995962299da68dad517be3e
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-08-04 18:16:25 +0500

    output

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   output: "export" /* config options here */,
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Tells Next.js to output a static site for export
 };
 
 export default nextConfig;
