66f56eebe804bbdd480a8b57a192c3d2e6f23148
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-08-07 21:55:47 +0500

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   output: "export" /* config options here */,
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Tells Next.js to output a static site for export
+  /* config options here */
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   output: "export", // Tells Next.js to output a static site for export
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,3 @@
-// import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
-// import Navbar from "./Components/navbar";
-// import Footer from "./Components/footer";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
-// export const metadata: Metadata = {
-//   title: "Channel link",
-//   description: " ",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en">
-//       <body
-//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-//       >
-//         <Navbar />
-//         {children}
-//         <Footer />
-//       </body>
-//     </html>
-//   );
-// }
-
 // app/layout.tsx
 import type { Metadata } from "next";
 // 1. Import 'localFont'
@@ -69,6 +30,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Channel link",
   description: " ",
+  icons: "/favicon.png",
 };
 
 export default function RootLayout({
